f913de2adee1536811d005e7fa58b8bba0f9ae54
Rebecca Congi <rncongi@gmail.com>
2020-08-06 19:36:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,6 @@ def sort_array_desc(nums)
   nums.sort{|a, b| b <=> a}
 end   
 
-def ssort_array_char_countort_array_char_count(strings)
+def sort_array_char_count(strings)
   strings.sort(|a, b| a.length <=> b.length)
 end 
