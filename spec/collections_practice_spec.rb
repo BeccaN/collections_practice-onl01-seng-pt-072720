e214f3f3020b7b7c6d32044949e0e7dc6df037ed
Rebecca Congi <rncongi@gmail.com>
2020-08-06 19:46:52 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,12 +30,12 @@ describe 'collections practice' do
     end
   end
   
-  # Question 4b
-  describe '#swap_elements_from_to' do 
-    it 'swaps the element at given index to second element at given index' do 
-      expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
-    end
-  end
+  # # Question 4b
+  # describe '#swap_elements_from_to' do 
+  #   it 'swaps the element at given index to second element at given index' do 
+  #     expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
+  #   end
+  # end
 
   # Question 5
   describe '#reverse_array' do
